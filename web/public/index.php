c5d5d33ed27281bf12c351730fbe427e118b31b2
minhtv <tvminh2409@gmail.com>
2017-06-30 13:27:19 +0700

    add index file

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,9 @@
-<?php
-
-include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
-
-?><!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
+        <title>Docker</title>
     </head>
     <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
+        <h1>Docker <?php echo phpinfo(); ?></h1>
     </body>
 </html>
